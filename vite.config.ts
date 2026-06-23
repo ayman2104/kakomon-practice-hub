aa3dcb775e72e751ce762d4a12523645b57163b8
ayman2104 <ayman2104@gmail.com>
2026-06-24 01:43:44 +0900

    Configure Netlify deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,11 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
-import { nitro } from "nitro/vite";
+import netlify from "@netlify/vite-plugin-tanstack-start";
 
 export default defineConfig({
   vite: {
-    plugins: [
-      nitro({
-        preset: "vercel",
-      }),
-    ],
+    plugins: [netlify()],
   },
+
   tanstackStart: {
     server: { entry: "server" },
   },
